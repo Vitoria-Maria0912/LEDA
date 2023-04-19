9d292fd85b183f52736ba7d75557167ad0851ba4
Vitória Maria do Nascimento <116987726+VitoriaMariaN@users.noreply.github.com>
2023-04-19 08:26:11 -0300

    Create CountingSort.java

---
+++ Roteiro 3/CountingSort.java
@@ -0,0 +1,29 @@
+package sorting.linearSorting;
+
+import sorting.AbstractSorting;
+
+/**
+ * Classe que implementa a estratégia de Counting Sort vista em sala.
+ *
+ * Procure evitar desperdício de memória: AO INVÉS de alocar o array de contadores
+ * com um tamanho arbitrariamente grande (por exemplo, com o maior valor de entrada possível),
+ * aloque este array com o tamanho sendo o máximo inteiro presente no array a ser ordenado.
+ *
+ * Seu algoritmo deve assumir que o array de entrada nao possui numeros negativos,
+ * ou seja, possui apenas numeros inteiros positivos e o zero.
+ *
+ */
+public class CountingSort extends AbstractSorting<Integer> {
+
+	@Override
+	public void sort(Integer[] array, int leftIndex, int rightIndex) {
+		if(leftIndex < 0 || leftIndex >= rightIndex || rightIndex >= array.length) {
+		} else {
+			coutingSort(array, rightIndex, rightIndex);
+		}
+	}
+
+	private void coutingSort(Integer[] array, int leftIndex, int rightIndex) {
+		
+	}
+}
