86fe91d17b57f314ab61084488710a381861eb14
Vitória Maria do Nascimento <116987726+VitoriaMariaN@users.noreply.github.com>
2023-04-26 18:52:13 -0300

    Update TesteFloor.java

--- Roteiro 4/Testes/TesteFloor.java
+++ Roteiro 4/Testes/TesteFloor.java
@@ -1,45 +1,42 @@
-class FloorBinarySearchTestes {
+package submission;
 
-    private FloorBinarySearchImpl implementacao;
-    private Integer[] array;
-    private Integer[] arrayOrdenado;
-    private Integer[] arrayVazio;
-    
-    @BeforeEach
-    void preparaArrays() {
-        this.implementacao = new FloorBinarySearchImpl();
-        this.array = new Integer[] {10, 27, -10, -5, 12, 100, 25, -20};
-        this.arrayOrdenado = new Integer[] {-20, -10, -5, 10, 12, 25, 27, 100};
-        this.arrayVazio = new Integer[] {};
-    }
-    
-    @Test
-    void testaOrdenacao() {
-        this.implementacao.floor(array, 12);
-        assertTrue(Arrays.equals(array, arrayOrdenado));
-    }
-    
-    @Test
-    void testaArrayVazio() {
-        int result = this.implementacao.floor(arrayVazio, -10);
-        assertEquals(-1, result);
-    }
-    
-@Test
-    void testaArray01() {
-        int result = this.implementacao.floor(array, -20);
-        assertEquals(-20, result);
-    }
-    
-    @Test
-    void testaArray02() {
-        int result = this.implementacao.floor(array, 100);
-        assertEquals(100, result);
-    }
-    
-    @Test
-    void testaArray03() {
-        int result = this.implementacao.floor(array, 11);
-        assertEquals(10, result);
-    }
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import problems.FloorBinarySearchImpl;
+
+public class TesteFloor {
+	
+	FloorBinarySearchImpl floorBinarySearch;
+	
+	@BeforeEach
+	void floor() {
+		this.floorBinarySearch = new FloorBinarySearchImpl();
+	}
+
+	@Test
+	public void test() {
+		Integer[] array = new Integer [] {1, 2, 5, 10};
+		int result = this.floorBinarySearch.floor(array, 4);
+		
+		assertEquals(2, result);
+	}
+	
+	@Test
+	public void test2() {
+		Integer[] array = new Integer [] {1, 2, 5, 10};
+		int result = this.floorBinarySearch.floor(array, 0);
+		
+		assertEquals(null, result);
+	}
+	
+	@Test
+	public void test3() {
+		Integer[] array = new Integer [] {};
+		int result = this.floorBinarySearch.floor(array, 1);
+		
+		assertNotEquals(2, result);
+	}
 }
