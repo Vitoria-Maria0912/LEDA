9525b7bf821dcc1fff5349710b7f08831e814691
Vitória Maria do Nascimento <116987726+VitoriaMariaN@users.noreply.github.com>
2023-04-26 09:51:23 -0300

    Update FloorBinarySearchImpl.java

--- Roteiro 4/FloorBinarySearchImpl.java
+++ Roteiro 4/FloorBinarySearchImpl.java
@@ -4,6 +4,35 @@ public class FloorBinarySearchImpl implements Floor {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		return 0;
+		
+		if(recursiveFloor(array, x, 0, array.length - 1) == -1) {
+			return null;
+			
+		} else {
+			return recursiveFloor(array, x, 0, array.length - 1);
+		}
+		
+	}
+
+	private Integer recursiveFloor(Integer[] array, Integer x, int left, int right) {
+		
+		int middle = (left + right) / 2;
+		
+		int floor = -1;
+		
+		if(left < right) {
+			if(array[middle] == x) {
+				floor = array[middle];
+				
+			} else if(array[middle] > x) {
+				recursiveFloor(array, x, left, middle - 1);
+				
+			} else {
+				recursiveFloor(array, x, middle + 1, right);
+				floor = array[middle];
+			}
+		}
+		
+		return floor;
 	}
 }
