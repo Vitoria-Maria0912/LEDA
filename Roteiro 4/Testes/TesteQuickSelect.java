83578ab747b5a9a03aa87358f508ce4514d74a7c
Vitória Maria do Nascimento <116987726+VitoriaMariaN@users.noreply.github.com>
2023-04-27 19:18:32 -0300

    Create TesteQuickSelect.java

---
+++ Roteiro 4/Testes/TesteQuickSelect.java
@@ -0,0 +1,67 @@
+package submission;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import orderStatistic.QuickSelect;
+
+class TesteQuickSelect {
+
+	QuickSelect<Integer> quick;
+	
+	@BeforeEach
+	void floor() {
+		this.quick = new QuickSelect<>();
+	}
+
+	@Test
+	public void test() {
+		try {
+			Integer[] array = new Integer [] {1, 2, 5, 10};
+			int result = quick.quickSelect(array, 0);
+			
+			assertEquals(null, result);
+		} catch(NullPointerException e) {
+		}
+	}
+
+	@Test
+	public void test2() {
+		try {
+			Integer[] array = new Integer [] {};
+			int result = quick.quickSelect(array, 1);
+			
+			assertEquals(null, result);
+		} catch(NullPointerException e) {
+		}
+	}
+	
+	@Test
+	public void test3() {
+		try {
+			Integer[] array = new Integer [] {1, 2, 5, 10};
+			int result = quick.quickSelect(array, 5);
+			
+			assertEquals(null, result);
+		} catch(NullPointerException e) {
+		}
+	}
+	
+	@Test
+	public void test4() {
+		Integer[] array = new Integer [] {1, 2, 5, 10};
+		int result = quick.quickSelect(array, 1);
+		
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void test5() {
+		Integer[] array = new Integer [] {1, 2, 5, 10};
+		int result = quick.quickSelect(array, 4);
+		
+		assertEquals(1, result);
+	}
+}
