5ba1291142d014f4354eec6e3a3e91fac8281ffe
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-04-30 23:45:42 -0300

    Update TesteQuickSelect.java

--- Roteiro 4/Testes/TesteQuickSelect.java
+++ Roteiro 4/Testes/TesteQuickSelect.java
@@ -62,17 +62,68 @@ class TesteQuickSelect {
 		Integer[] array = new Integer [] {1, 2, 5, 10};
 		int result = quick.quickSelect(array, 4);
 		
-		assertEquals(1, result);
+		assertEquals(10, result);
 	}
 	
 	@Test
 	public void test6() {
 		try {
-			Integer[] array = new Integer [] {null};
-			int result = quick.quickSelect(array, 1);
+			Integer[] array = new Integer [] {1, 4};
+			int result = quick.quickSelect(array, 3);
 			
 			assertEquals(null, result);
 		} catch(NullPointerException e) {
 		}
+	}
+	
+	@Test
+	public void test7() {
+		Integer[] array = new Integer [] {5, 10, 0, 2};
+		int result = quick.quickSelect(array, 1);
+		
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void test8() {
+		try {
+			Integer[] array = new Integer [] {1, 2, 3};
+			int result = quick.quickSelect(array, -1);
+			
+			assertEquals(null, result);
+		} catch(NullPointerException e) {
+		}
+	}
+	
+	@Test
+	public void test9() {
+		Integer[] array = new Integer [] {1, 2, 3, 5, 10};
+		int result = quick.quickSelect(array, 3);
+		
+		assertEquals(3, result);
+	}
+	
+	@Test
+	public void test10() {
+		Integer[] array = new Integer [] {1, 2, 3, 5, 10, 15};
+		int result = quick.quickSelect(array, 6);
+		
+		assertEquals(15, result);
+	}
+	
+	@Test
+	public void test11() {
+		Integer[] array = new Integer [] {5, 10, 0, 2};
+		int result = quick.quickSelect(array, 2);
+		
+		assertEquals(2, result);
+	}
+	
+	@Test
+	public void test12() {
+		Integer[] array = new Integer [] {15, 14, 13, 12, 11, 10};
+		int result = quick.quickSelect(array, 1);
+		
+		assertEquals(10, result);
 	}
 }
