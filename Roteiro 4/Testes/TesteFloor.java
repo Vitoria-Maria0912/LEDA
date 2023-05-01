b64a6ef60bd89d838b7b5cd2ef58ce2f38e18739
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-04-30 21:40:38 -0300

    Update TesteFloor.java

--- Roteiro 4/Testes/TesteFloor.java
+++ Roteiro 4/Testes/TesteFloor.java
@@ -3,40 +3,129 @@ package submission;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 import problems.FloorBinarySearchImpl;
 
 public class TesteFloor {
 	
-	FloorBinarySearchImpl floorBinarySearch;
-	
-	@BeforeEach
-	void floor() {
-		this.floorBinarySearch = new FloorBinarySearchImpl();
-	}
-
 	@Test
 	public void test() {
+		FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
 		Integer[] array = new Integer [] {1, 2, 5, 10};
-		int result = this.floorBinarySearch.floor(array, 4);
+		int result = floorBinarySearch.floor(array, 4);
 		
 		assertEquals(2, result);
 	}
 	
 	@Test
 	public void test2() {
-		Integer[] array = new Integer [] {1, 2, 5, 10};
-		int result = this.floorBinarySearch.floor(array, 0);
-		
-		assertEquals(null, result);
+		try {
+			FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+			Integer[] array = new Integer [] {1, 2, 5, 10};
+			int result = floorBinarySearch.floor(array, 0);
+			
+			assertEquals(null, result);
+		} catch (NullPointerException e) {
+		}
 	}
 	
 	@Test
 	public void test3() {
-		Integer[] array = new Integer [] {};
-		int result = this.floorBinarySearch.floor(array, 1);
+		try {
+			FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+			Integer[] arrayVazio = new Integer [] {};
+			int result = floorBinarySearch.floor(arrayVazio, 1);
+			
+			assertNotEquals(null, result);
+		} catch(NullPointerException e) {
+			
+		}
+	}
+	
+	@Test
+	public void test4() {
+		FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+		Integer[] array = new Integer [] {1, 2, 4, 5};
+		int result = floorBinarySearch.floor(array, 5);
 		
-		assertNotEquals(2, result);
+		assertEquals(5, result);
+	}
+	
+	@Test
+	public void test5() {
+		FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+		Integer[] array = new Integer [] {1, 2, 4, 5, 7};
+		int result = floorBinarySearch.floor(array, 4);
+		
+		assertNotEquals(2, result); 
+	}
+
+	@Test
+	public void test6() {
+		FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+		Integer[] array = new Integer [] {1, 2, 4, 5, 7};
+		int result = floorBinarySearch.floor(array, 8);
+		
+		assertEquals(7, result);
+	}
+	
+	@Test
+	public void test7() {
+		FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+		Integer[] array = new Integer [] {7, 6, 2, 0};
+		int result = floorBinarySearch.floor(array, 8);
+		
+		assertEquals(7, result);
+	}
+	
+	@Test
+	public void test8() {
+		FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+		Integer[] array = new Integer [] {10, 0, 60, -1};
+		int result = floorBinarySearch.floor(array, 4);
+		
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void test9() {
+		try {
+			FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+			Integer[] array = new Integer [] {1};
+			int result = floorBinarySearch.floor(array, 0);
+			
+			assertEquals(null, result);
+		} catch (NullPointerException e) {
+		}
+	}
+	
+	@Test
+	public void test10() {
+		FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+		Integer[] array = new Integer [] {-2};
+		int result = floorBinarySearch.floor(array, 0);
+		
+		assertEquals(-2, result);
+	}
+	
+	@Test
+	public void test11() {
+		FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+		Integer[] array = new Integer [] {-2, 1};
+		int result = floorBinarySearch.floor(array, 0);
+		
+		assertEquals(-2, result);
+	}
+	
+	@Test
+	public void test12() {
+		try {
+			FloorBinarySearchImpl floorBinarySearch = new FloorBinarySearchImpl();
+			Integer[] array = new Integer [] {-2, 1, 7};
+			int result = floorBinarySearch.floor(array, -3);
+			
+			assertEquals(null, result);
+		} catch (NullPointerException e) {
+		}
 	}
 }
