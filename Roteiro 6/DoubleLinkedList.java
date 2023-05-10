866d8971f6fed47573504505062c33a7c43c49ec
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-05-09 22:55:49 -0300

    Update DoubleLinkedList.java

--- Roteiro 6/DoubleLinkedList.java
+++ Roteiro 6/DoubleLinkedList.java
@@ -12,29 +12,69 @@ public class DoubleLinkedListImpl<T> extends SingleLinkedListImpl<T> implements
 
 	@Override
 	public void insertFirst(T element) {
-		
+		if(element != null) {
+			
+			DoubleLinkedListNode<T> node = new DoubleLinkedListNode<>();
+			
+			setData(element);
+			setNext(getHead());
+			setPrevious(new DoubleLinkedListNode<T>());
+
+			// daqui para baixo não entendi nada
+			((DoubleLinkedListNode<T>) this.getHead()).setPrevious(node);
+			
+			if(getHead().isNIL()) {
+				this.setLast(node);
+			}
+			this.setHead(node);
+		}
 	}
 
 	@Override
 	public void removeFirst() {
-		
+		if(!(this.isEmpty())) {
+		}
 	}
 
 	@Override
 	public void removeLast() {
+		if(!(this.isEmpty())) {
+		}
 	}
 
 	@Override
 	public void insert(T element) {
+		if(element != null) {
+		}
 	}
 
 	@Override
 	public void remove(T element) {
+		if(!(this.isEmpty() && element != null) {
+		}
 	}
 
 	@Override
 	public T search(T element) {
-
+		
+		T found = null;
+		
+		if(!(this.isEmpty() && element != null) {
+			
+			DoubleLinkedListNode<T> auxHead = (DoubleLinkedListNode<T>) this.getHead();
+			DoubleLinkedListNode<T> auxLast = this.getLast();
+			
+			while(!(auxHead.equals(auxLast)) && !(auxHead.getData().equals(element)) && !(auxLast.getData().equals(element))) {
+				auxHead = auxHead.getNext(); // tem que fazer o cast?
+				auxLast = auxLast.getPrevious();
+			}
+			if(auxHead.getData().equals(element)) {
+				found = auxHead.getData();
+				
+			} else if(auxLast.getData().equals(element)) { // acho que tem o "else", porque se um ocorrer o outro não precisa ser checado
+				found = auxLast.getData();
+			}
+		}
 		return found;
 	}
 
