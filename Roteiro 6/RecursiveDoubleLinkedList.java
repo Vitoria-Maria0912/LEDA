d3a7335357aeb7125229dd8552688488b745f6dd
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2023-05-20 18:47:44 -0300

    Update RecursiveDoubleLinkedList.java

--- Roteiro 6/RecursiveDoubleLinkedList.java
+++ Roteiro 6/RecursiveDoubleLinkedList.java
@@ -6,30 +6,78 @@ public class RecursiveDoubleLinkedListImpl<T> extends
 	protected RecursiveDoubleLinkedListImpl<T> previous;
 
 	public RecursiveDoubleLinkedListImpl() {
-	}
 
-	@Override
-	public void insert(T element) {
 	}
 
 	@Override
 	public void insertFirst(T element) {
-	}
+		if(element != null) {
+			
+			if(isEmpty()) {
+				insert(element);
+				
+			} else {
+				
+				RecursiveDoubleLinkedListImpl<T> node = new RecursiveDoubleLinkedListImpl<>();
+						
+				node.setData(getData());
+				node.setNext(getNext());
+				node.setPrevious(this);
+				
+				setData(element);
+				setNext(node);
+				((RecursiveDoubleLinkedListImpl<T>) getNext()).setPrevious(node);
+			}
 
-	@Override
-	public void remove(T element) {
+		}
 	}
 
 	@Override
 	public void removeFirst() {
-		
+		if(!(isEmpty())) {
+			
+			if(getNext().isEmpty() && getPrevious().isEmpty()) {
+				setData(null);
+
+			} else {
+				setData(getNext().getData());
+				setNext(getNext().getNext());
+				
+				((RecursiveDoubleLinkedListImpl<T>) getNext()).setPrevious(this);
+			}
+		}
 	}
 
 	@Override
 	public void removeLast() {
-		
+		if(!(isEmpty())) {
+			if(getNext().isEmpty()) {
+				setData(null);
+			
+			} else {
+				((RecursiveDoubleLinkedListImpl<T>) getNext()).removeLast();
+			}
+		}
 	}
-
+	
+	@Override
+	public void insert(T element) {
+		
+		if(element != null) {
+			
+			if(isEmpty()) {
+				setData(element);
+				setNext(new RecursiveDoubleLinkedListImpl<T>());
+				
+				if (this.previous == null) {
+					setPrevious(new RecursiveDoubleLinkedListImpl<T>());
+				}
+			} else {
+				getNext().insert(element);			
+			}
+		}
+	}
+	
 	public RecursiveDoubleLinkedListImpl<T> getPrevious() {
 		return this.previous;
 	}
